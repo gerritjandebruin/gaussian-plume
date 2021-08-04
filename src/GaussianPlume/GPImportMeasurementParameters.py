1d5caef4b717e28755cfb52dc0da93bafcbb5a73
Robbert Bloem <robbert.bloem@tno.nl>
2021-08-04 17:23:20 +0200

    Added function to import parameters from Excel, including documentation and tests

--- src/GaussianPlume/GPImportMeasurementParameters.py
+++ src/GaussianPlume/GPImportMeasurementParameters.py
@@ -1,4 +1,52 @@
 """
+Import parameter files.
+
+Parameters can be static or dynamic. Static parameters don't change during the measurement, for example a device used during the measurement. A dynamic parameter does change over time. This can for example be the terrain roughness. 
+
+Excel
+-----
+Static parameters are stored in a sheet named `static parameters' (the sheet names are important). Column A contains the parameter names, column B the parameter values. If a parameter name or value is missing, it is ignored.
+
+This data:
+
+============ =======
+Device       TNO XYZ
+Operator      
+Stack height 5
+============ =======
+
+Will be read as:
+
+============ =======
+Device       TNO XYZ
+Stack height 5
+============ =======
+
+Dynamic parameters are stored in a sheet named "dynamic parameters". Column A contains a time stamp, the other columns the parameters. Row 1 contains the parameter names. The idea is that you only need to fill in the value of the parameter that has changed. If a value is missing, it will use the last valid value, if there is none, it will be set to NaN. 
+
+This data:
+
+================ ========= =========
+datetime         Roughness Windspeed
+================ ========= =========
+02/08/2018 7:00            5
+02/08/2018 9:40  C         
+02/08/2018 10:33           11
+02/08/2018 11:22 E          	
+02/08/2018 12:25 F         16
+================ ========= =========
+
+Will be read as:
+
+================ ========= =========
+datetime         Roughness Windspeed
+================ ========= =========
+02/08/2018 7:00  **NaN**   5
+02/08/2018 9:40  C         **5**
+02/08/2018 10:33 **C**     11
+02/08/2018 11:22 E         **11**	
+02/08/2018 12:25 F         16
+================ ========= =========
 
 
 """
@@ -7,6 +55,7 @@ import importlib
 import pathlib
 import numpy
 import pandas
+import warnings
 
 def import_measurement_parameters_txt(paf):
     """
@@ -42,7 +91,7 @@ def parse_lines_txt(line, parameters, array):
     
     
 
-def import_measurement_parameters_excel(paf, fixed_parameters = True, dynamic_parameters = True):
+def import_measurement_parameters_excel(paf, static_parameters = True, dynamic_parameters = True, verbose = 0):
     """
     
     
@@ -50,25 +99,76 @@ def import_measurement_parameters_excel(paf, fixed_parameters = True, dynamic_pa
     ---------
     paf : pathlib.Path
         Path and filename of the Excel file with parameters
-    fixed_parameters : Bool (True)
-        Read the fixed parameters sheet in the Excel file
-    dynamic_parameters : Bool (True)
-        Read the dynamic parameters sheet in the Excel file
+    static_parameters : bool (True) or str
+        Read the fixed parameters sheet in the Excel file. If this is a string, it will be used as the sheet name.
+    dynamic_parameters : bool (True) or str
+        Read the dynamic parameters sheet in the Excel file. If this is a string, it will be used as the sheet name.
+    
+    Returns
+    -------
+    Pandas dataframe or None
+        DataFrame with static parameters. None if static_parameters is False
+    Pandas dataframe or None
+        DataFrame with dynamic parameters.None if dynamic_parameters is False
+    
+    Raises
+    ------
+    ValueError
+        If the sheet name is incorrect.
+    UserWarning
+        If the file is not found at the paf. 
     
     """
+    if verbose > 1:
+        print("GPImportMeasurementParameters.import_measurement_parameters_excel()")
+    if verbose > 2:
+        print("    paf (path and filename): {:}".format(paf))
+        print("    static_parameters: {:}".format(static_parameters))
+        print("    dynamic_parameters: {:}".format(dynamic_parameters))
+    
+    if paf.exists() == False:
+        warnings.warn("GPImportMeasurementParameters.import_measurement_parameters_excel(): parameter file does not exist (at this location): {:}".format(paf))
+        return None, None
     
     with open(paf, "rb") as F:
-        if fixed_parameters:
-            df_fixed = pandas.read_excel(F, "fixed parameters", index_col = 0, header = None) 
-        if dynamic_parameters:
-            df_dynamic = pandas.read_excel(F, "dynamic parameters", header = 0, parse_dates = ["datetime"]) 
-
-    df_fixed = df_fixed.transpose().reset_index(drop = True)
+    
+        if static_parameters:
+            if type(static_parameters) == str:
+                sheetname = static_parameters
+            else:
+                sheetname = "static parameters"
+        
+            df_static = pandas.read_excel(F, sheetname, index_col = 0, header = None) 
             
-    print(df_fixed)
-    # print("---")
-    # print(df_dynamic)
-    # print(df_dynamic.dtypes)
+            # transpose data and make a new index
+            df_static = df_static.transpose()
+            df_static = df_static.reset_index(drop = True)
+            
+            # remove parameters without a value
+            for cl in df_static.columns:
+                if pandas.isna(df_static.loc[0,cl]): 
+                    df_static.drop(cl, axis=1, inplace = True)            
+            
+        else:
+            df_static = None
+
+
+        if dynamic_parameters:
+            if type(dynamic_parameters) == str:
+                sheetname = dynamic_parameters
+            else:
+                sheetname = "dynamic parameters"        
+        
+            df_dynamic = pandas.read_excel(F, sheetname, header = 0, parse_dates = ["datetime"]) 
+            
+            # forward fill data
+            df_dynamic.fillna(method = "ffill", inplace = True) 
+        else:
+            df_dynamic = None
+          
+    return df_static, df_dynamic
+            
+
     
     
     
--- tests/GPImportMeasurementParameters_Tests.py
+++ tests/GPImportMeasurementParameters_Tests.py
@@ -1,8 +1,11 @@
 import importlib 
 import pathlib
 import numpy
+import pandas
 import unittest
 import warnings
+
+
 
 import GPImportMeasurementParameters as GPIMP
 
@@ -16,7 +19,7 @@ class Test_import_measurement_parameters_txt(unittest.TestCase):
     def test_basic(self):
         
         paf = pathlib.Path(r"C:\Python\GaussianPlume\tests\testdata\inputfiles\configuration_1.txt")
-        print(paf)
+        # print(paf)
         GPIMP.import_measurement_parameters_txt(paf)
 
 
@@ -28,9 +31,57 @@ class Test_import_measurement_parameters_excel(unittest.TestCase):
     def test_basic(self):
         
         paf = pathlib.Path(r"C:\Python\GaussianPlume\tests\testdata\inputfiles\configuration_1.xlsx")
-        print(paf)
-        GPIMP.import_measurement_parameters_excel(paf)
+        df_static, df_dynamic = GPIMP.import_measurement_parameters_excel(paf, verbose = self.verbose)
+        
+        # empty parameter was removed
+        self.assertFalse("var C" in df_static.columns)
 
+        # first row is NaN
+        self.assertTrue(pandas.isna(df_dynamic.loc[0,"roughness"]))
+        self.assertFalse(pandas.isna(df_dynamic.loc[1,"roughness"]))
+        self.assertFalse(pandas.isna(df_dynamic.loc[2,"roughness"]))
+        self.assertTrue(numpy.all(df_dynamic.loc[1:,"roughness"] == ["D", "D", "E", "F"]))
+
+        self.assertFalse(pandas.isna(df_dynamic.loc[2,"windspeed"]))
+        self.assertFalse(pandas.isna(df_dynamic.loc[3,"windspeed"]))
+        self.assertTrue(numpy.all(df_dynamic.loc[:,"windspeed"] == [5,7,11,11,16]))
+
+    def test_no_static(self):
+        
+        paf = pathlib.Path(r"C:\Python\GaussianPlume\tests\testdata\inputfiles\configuration_1.xlsx")
+        df_static, df_dynamic = GPIMP.import_measurement_parameters_excel(paf, static_parameters = False, dynamic_parameters = True, verbose = self.verbose)
+        self.assertTrue(df_static is None)
+
+
+    def test_no_dynamic(self):
+        
+        paf = pathlib.Path(r"C:\Python\GaussianPlume\tests\testdata\inputfiles\configuration_1.xlsx")
+        df_static, df_dynamic = GPIMP.import_measurement_parameters_excel(paf, static_parameters = True, dynamic_parameters = False, verbose = self.verbose)
+        self.assertTrue(df_dynamic is None)        
+
+    def test_wrong_paf(self):
+        
+        paf = pathlib.Path(r"C:\Python\GaussianPlume\tests\testdata\inputfiles\does_not_exist.xlsx")
+        
+        # test without the path
+        test = "GPImportMeasurementParameters.import_measurement_parameters_excel(): parameter file does not exist (at this location):"
+        test_length = len(test)
+        
+        with warnings.catch_warnings(record=True) as w:
+            df_static, df_dynamic = GPIMP.import_measurement_parameters_excel(paf, verbose = self.verbose)
+            self.assertTrue(issubclass(w[-1].category, UserWarning))
+
+        self.assertEqual(str(w[0].message)[:test_length], test)
+        
+    def test_wrong_sheetname(self):
+        
+        paf = pathlib.Path(r"C:\Python\GaussianPlume\tests\testdata\inputfiles\configuration_1.xlsx")
+        
+        
+        with self.assertRaises(ValueError) as cm:
+            df_static, df_dynamic = GPIMP.import_measurement_parameters_excel(paf, static_parameters = "wrong sheet name",verbose = self.verbose)
+        self.assertEqual(str(cm.exception), "Worksheet named 'wrong sheet name' not found")
+        
         
 if __name__ == '__main__': 
     verbosity = 1
