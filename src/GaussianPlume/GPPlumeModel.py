35f1f48db19be7a8178eea0cde5ef9112cf37cee
Robbert Bloem <robbert.bloem@tno.nl>
2021-08-10 07:24:08 +0200

    Implementing methods to check and/or calculate values needed for the concentration calculation

--- src/GaussianPlume/GPPlumeModel.py
+++ src/GaussianPlume/GPPlumeModel.py
@@ -30,10 +30,12 @@ Some are constants
 import importlib
 import pathlib
 
+import GPConstants as GPC
 import GPFunctions as GPF
 import GPImportData as GPID
 import PythonTools.ClassTools as CT
 
+importlib.reload(GPC)
 importlib.reload(GPF)
 importlib.reload(GPID)
 
@@ -60,7 +62,11 @@ class Plume(CT.ClassTools):
     latM : number-like  
         Latitude of the measurement.
     lonM : number-like  
-        Longitude of the measurement.               
+        Longitude of the measurement.   
+    latR : number-like  
+        Latitude of the reference.
+    lonR : number-like  
+        Longitude of the reference.           
     z0 : number-like
         Source height in meters
     Zr : number-like
@@ -99,21 +105,27 @@ class Plume(CT.ClassTools):
         self.measurement_path = kwargs.get("measurement_path", None)
         self.measurement_paf = GPF.handle_filename_path(filename = self.measurement_filename, path = self.measurement_path, verbose = self.verbose)
 
-        self.qs = kwargs.get("qs", None)
-        self.wind_speed = kwargs.get("wind_speed", None)
-        self.wind_direction = kwargs.get("wind_direction", None)
-        self.latS = kwargs.get("latS", None)
-        self.lonS = kwargs.get("lonS", None)
-        self.latM = kwargs.get("latM", None)
-        self.lonM = kwargs.get("lonM", None)
-        self.dx = kwargs.get("dx", None)
-        self.dy = kwargs.get("dy", None)
-        self.z0 = kwargs.get("z0", None)
-        self.zr = kwargs.get("zr", None)
-        self.hs = kwargs.get("hs", None)
-        self.hm = kwargs.get("hm", None)
+        self.dispersion_constants = kwargs.get("dispersion_constants", None)
+
+        self.molecules = kwargs.get("molecules", None)
+        self.molecular_properties = kwargs.get("molecules_properties", None)
         self.molecular_mass = kwargs.get("molecular_mass", None)
         self.mode = kwargs.get("mode", None)
+        
+        # self.qs = kwargs.get("qs", None)
+        # self.wind_speed = kwargs.get("wind_speed", None)
+        # self.wind_direction = kwargs.get("wind_direction", None)
+        # self.latS = kwargs.get("latS", None)
+        # self.lonS = kwargs.get("lonS", None)
+        # self.latM = kwargs.get("latM", None)
+        # self.lonM = kwargs.get("lonM", None)
+        # self.dx = kwargs.get("dx", None)
+        # self.dy = kwargs.get("dy", None)
+        # self.z0 = kwargs.get("z0", None)
+        # self.zr = kwargs.get("zr", None)
+        # self.hs = kwargs.get("hs", None)
+        # self.hm = kwargs.get("hm", None)
+
         if "stability_index" in kwargs:
             self.stability_index = kwargs["stability_index"]
         elif "stability_class" in kwargs:
@@ -146,27 +158,176 @@ class Plume(CT.ClassTools):
 
 
 
-    def import_data(self, static_parameters = True, dynamic_parameters = True, verbose = 0, **kwargs):
+    def import_data(self, verbose = 0, **kwargs):
         """
+        Import static and dynamic parameters, and the measurement data.
+        
+        Arguments
+        ---------
+        **kwargs : dict, optional
+            May include `parameter_paf`, `measurement_paf`, `static_parameters`, `dynamic_parameters`
+        
+        Notes
+        -----
+        There are three ways to give `measurement_paf`, i.e. the list with path-and-filenames of the measurement files.
+        
+        1. As kwarg when calling this function.
+        2. As an defined in the PlumeModel object earlier.
+        3. As the parameter `measurement_data_path_and_filename` in the static parameters.
+        
+        
         
         """
         verbose = GPF.print_vars(function_name = "GPPlumeModel.import_data()", function_vars = vars(), verbose = verbose, self_verbose = self.verbose)
 
         parameter_paf = kwargs.get("parameter_paf", self.parameter_paf)
         measurement_paf = kwargs.get("measurement_paf", self.measurement_paf)
+        
+        static_parameters = kwargs.get("static_parameters", True)
+        dynamic_parameters = kwargs.get("dynamic_parameters", True)
 
         df_static, df_dynamic = GPID.import_measurement_parameters_excel(parameter_paf, static_parameters = static_parameters, dynamic_parameters = dynamic_parameters, verbose = verbose)
 
         if measurement_paf is None:
             measurement_paf = [pathlib.Path(df_static.loc[0,"measurement_data_path_and_filename"])]
+        
+        self.measurement_paf = measurement_paf
 
         df = GPID.import_measurement_data(measurement_paf, verbose = verbose)
 
         self.df = GPID.merge_measurement_static_dynamic_df(df, df_static, df_dynamic, verbose = verbose)
+        
+        for col_name in self.df.columns:
+            if col_name in GPC.implemented_molecules:
+                if self.molecules is None:
+                    self.molecules = [col_name]
+                else:
+                    self.molecules.append(col_name)
 
 
+    
+    def calculate_concentration_prepare_dxdy(self, verbose = 0):
+        """
+        Check if `dx` or `dy` exists in the DataFrame, if either does not exist, calculate `dx` and `dy`.
+        
+        Raises
+        ------
+        ValueError
+            If `dx` or `dy` does not exist, and "latS" or "lonS" or "latM" or "lonM" or "latR" or "lonR" or "wind_direction" do not exist either.
+        
+        """
+        verbose = GPF.print_vars(function_name = "GPPlumeModel.calculate_concentration_prepare_dxdy()", function_vars = vars(), verbose = verbose, self_verbose = self.verbose)
+        
+        cn = self.df.columns
+        if "dx" not in cn or "dy" not in cn:
+            if "latS" in cn and "lonS" in cn and "latM" in cn and "lonM" in cn and "latR" in cn and "lonR" in cn and "wind_direction" in cn:
+                dlatM, dlonM = GPF.latlon2dlatdlon(self.df.loc[:,"latM"], self.df.loc[:,"lonM"], self.df.loc[:,"latR"], self.df.loc[:,"lonR"], verbose = verbose)
+                dlatS, dlonS = GPF.latlon2dlatdlon(self.df.loc[:,"latS"], self.df.loc[:,"lonS"], self.df.loc[:,"latR"], self.df.loc[:,"lonR"], verbose = verbose)
+                dx, dy = GPF.dlatdlon2dxdy(dlatS, dlonS, dlatM, dlonM, self.df.loc[:,"wind_direction"], verbose = verbose)
+                
+                idx = len(self.df.columns)
+                self.df.insert(idx, "dy", dy)
+                self.df.insert(idx, "dx", dx)
+            else:
+                print("GPPlumeModel.calculate_concentration_prepare_dxdy(): dx is missing and can not be calculated.")
+                raise ValueError
 
 
+        
+    def calculate_concentration_prepare_tc(self, verbose = 0):
+        """
+        Check if `tc` exists in the DataFrame, if it doesn't, calculate `tc`.
+        
+        Raises
+        ------
+        ValueError
+            If `tc` does not exist, and "dx" or "wind_direction" do not exist either.        
+        """
+        verbose = GPF.print_vars(function_name = "GPPlumeModel.calculate_concentration_prepare_tc()", function_vars = vars(), verbose = verbose, self_verbose = self.verbose)
+        
+        cn = self.df.columns
+        if "tc" not in cn:
+            if "dx" in cn and "wind_speed" in cn :
+                tc = GPF.calculate_Tc(self.df.loc[:,"dx"], self.df.loc[:,"wind_speed"], verbose = verbose)
+                idx = len(self.df.columns)
+                self.df.insert(idx, "tc", tc)
+            else:
+                print("GPPlumeModel.calculate_concentration_prepare_tc(): tc is missing and can not be calculated.")
+                raise ValueError        
+
+
+    def calculate_concentration_prepare_dispersion_constants(self, verbose = 0):
+        """
+        Check if `dispersion_constants` exists in the DataFrame, if it doesn't, import it from `GPConstants`.
+        
+        Raises
+        ------
+        ValueError
+            If `dispersion_constants` does not exist, and "mode" does not exist either.          
+        """
+        verbose = GPF.print_vars(function_name = "GPPlumeModel.calculate_concentration_prepare_dispersion_constants()", function_vars = vars(), verbose = verbose, self_verbose = self.verbose)
+
+        if self.dispersion_constants is None:
+            if self.mode is not None:
+                self.dispersion_constants = GPF.get_dispersion_constants(mode = self.mode, verbose = verbose)
+            else:
+                print("GPPlumeModel.calculate_concentration_prepare_dispersion_constants(): dispersion_constants and mode can not be set.")
+                raise ValueError      
+
+    def calculate_concentration_get_molecular_properties(self, verbose = 0): 
+        """
+        Check if `molecular_mass` is set. If it doesn't, import it from `GPConstants`.
+        
+        Raises
+        ------
+        ValueError
+            If `molecular_mass` does not exist, and "molecules" does not exist either.                  
+        """
+        verbose = GPF.print_vars(function_name = "GPPlumeModel.calculate_concentration_get_molecular_weights()", function_vars = vars(), verbose = verbose, self_verbose = self.verbose)
+
+        if self.molecules is None or len(self.molecules) == 0:
+            print("GPPlumeModel.calculate_concentration_get_molecular_mass(): molecules are not known.")
+            raise ValueError  
+        
+        if self.molecular_mass is None:
+            self.molecular_properties = []
+            self.molecular_mass = []
+            for m_i, m in enumerate(self.molecules):
+                m_prop = GPF.get_molecule_properties(m, verbose = verbose)
+                self.molecular_properties.append(m_prop)
+                self.molecular_mass.append(m_prop["molecular_mass"])
+
+
+    # def calculate_sigma(self):
+        # """
+        
+        # """
+
+        
+        # GPF.calculate_sigma(dx = self.df["x"], z0 = self.df["z0"], Tc = self.df["tc"], dispersion_constants = 1, stability = , verbose = 0, **kwargs)
+
+
+    def calculate_concentration(self, verbose = 0, **kwargs):
+        """
+        
+        
+        """
+        verbose = GPF.print_vars(function_name = "GPPlumeModel.calculate_concentration()", function_vars = vars(), verbose = verbose, self_verbose = self.verbose)
+        
+        self.calculate_concentration_prepare_dxdy(verbose = verbose)
+        self.calculate_concentration_prepare_tc(self, verbose = verbose)
+        self.calculate_concentration_prepare_dispersion_constants(self, verbose = verbose)
+        self.calculate_concentration_get_molecular_properties(self, verbose = verbose)
+
+
+    
+    
+
+        
+        
+        
+        
+        
 
 
 
--- tests/GPPlumeModel_Tests.py
+++ tests/GPPlumeModel_Tests.py
@@ -1,8 +1,10 @@
 import importlib 
+from io import StringIO
 import pathlib
 import numpy
 import pandas
 import unittest
+from unittest.mock import patch
 import warnings
 
 
@@ -35,10 +37,191 @@ class Test_basic(unittest.TestCase):
         
         print(P.stability_class)
         
+
+class Test_calculate_concentration_prepare(unittest.TestCase):
+
+    def setUp(self):
+        self.verbose = 1
+        
+    def test_basic_dxdy(self):
+        """
+        Most basic functionality. 
+
+        """
+        data = {
+            "dx": numpy.array([-296.06762177, -49.9696837]),
+            "dy": numpy.array([-412.86479599, -505.58521254]),
+        }
+        
+        df = pandas.DataFrame(data = data)
+        
+        P = GPPM.Plume(verbose = 0, df = df)
+                
+        P.calculate_concentration_prepare_dxdy()
+        
+        test_dx = [-296.06762177, -49.9696837]
+        test_dy = [-412.86479599, -505.58521254]
+
+        self.assertTrue(numpy.allclose(P.df["dx"].to_numpy(), test_dx))
+        self.assertTrue(numpy.allclose(P.df["dy"].to_numpy(), test_dy))
+        
+
+    def test_basic_dxdy_calculate(self):
+        """
+        Most basic functionality. 
+
+        """
+        data = {
+            "latS": numpy.array([53.2835, 53.2835]),
+            "lonS": numpy.array([6.3039, 6.3039]),
+            "latM": numpy.array([53.2876717, 53.2876717]),
+            "lonM": numpy.array([6.3007517, 6.3007517]),
+            "latR": numpy.array([53.28375, 53.28375]),
+            "lonR": numpy.array([6.3024917, 6.3024917]),     
+            "wind_direction": numpy.array([30,60]),
+        }
+        
+        df = pandas.DataFrame(data = data)
+        
+        P = GPPM.Plume(verbose = 0, df = df)
+                
+        P.calculate_concentration_prepare_dxdy()
+        
+        test_dx = [-296.06762177, -49.9696837]
+        test_dy = [-412.86479599, -505.58521254]
+
+        self.assertTrue(numpy.allclose(P.df["dx"].to_numpy(), test_dx))
+        self.assertTrue(numpy.allclose(P.df["dy"].to_numpy(), test_dy))
+        
+
+
+    def test_basic_dxdy_missing_column(self):
+        """
+        Most basic functionality. 
+
+        """
+        data = {
+            "latS": numpy.array([53.2835, 53.2835]),
+            "lonS": numpy.array([6.3039, 6.3039]),
+            "latM": numpy.array([53.2876717, 53.2876717]),
+            "lonM": numpy.array([6.3007517, 6.3007517]),
+            "latR": numpy.array([53.28375, 53.28375]),
+            "lonR": numpy.array([6.3024917, 6.3024917]),     
+        }
+        
+        df = pandas.DataFrame(data = data)
+        
+        P = GPPM.Plume(verbose = 0, df = df)
+        
+        test_string = "GPPlumeModel.calculate_concentration_prepare_dxdy(): dx is missing and can not be calculated."
+        with self.assertRaises(ValueError) as cm:
+            with patch('sys.stdout', new_callable = StringIO) as mock_stdout:
+                P.calculate_concentration_prepare_dxdy()
+            self.assertTrue(mock_stdout.getvalue() == test_string)       
+
+
+    def test_basic_tc(self):
+        """
+        Most basic functionality. 
+
+        """
+        data = {
+            "tc": numpy.array([100, 200]),
+            
+        }
+        
+        df = pandas.DataFrame(data = data)
+        
+        P = GPPM.Plume(verbose = 0, df = df)
+                
+        P.calculate_concentration_prepare_tc()
+
+        
+        test_tc = numpy.array([100, 200])
+        
+        self.assertTrue(numpy.allclose(P.df["tc"].to_numpy(), test_tc))
+        
+
+    def test_basic_tc_calculate(self):
+        """
+        Most basic functionality. 
+
+        """
+        data = {
+            "dx": numpy.array([296.06762177, 49.9696837]),
+            "wind_speed": numpy.array([10, 20]),
+        }
+        df = pandas.DataFrame(data = data)
+        P = GPPM.Plume(verbose = 0, df = df)
+        P.calculate_concentration_prepare_tc()
+        test_tc = numpy.array([0.0082241, 0.00069402])
+        self.assertTrue(numpy.allclose(P.df["tc"].to_numpy(), test_tc))
+
+    def test_calculate_concentration_prepare_dispersion_constants(self):
+        """
+        Most basic functionality. 
+
+        """
+        dc = numpy.array([[0.1, 0.2], [0.3, 0.4]]),
+        P = GPPM.Plume(verbose = 0, dispersion_constants = dc)
+        P.calculate_concentration_prepare_dispersion_constants()
+        test_dc = numpy.array([[0.1, 0.2], [0.3, 0.4]])
+        self.assertTrue(numpy.allclose(P.dispersion_constants, test_dc))
+
+
+    def test_calculate_concentration_prepare_dispersion_constants_get(self):
+        """
+        Most basic functionality. 
+
+        """
+        mode = "nogepa"
+        P = GPPM.Plume(verbose = 0, mode = mode)
+        P.calculate_concentration_prepare_dispersion_constants()
+        
+        dispersion = numpy.array([
+            [1.36,  0.866, 0.23, 0.85],
+            [0.768, 0.897, 0.22, 0.8],
+            [0.47,  0.907, 0.2,  0.76],
+            [0.359, 0.902, 0.15, 0.73],
+            [0.238, 0.902, 0.12, 0.67],
+            [0.2,   0.902, 0.1,  0.62],
+        ])
+        
+        self.assertTrue(numpy.allclose(P.dispersion_constants, dispersion))
+
+    def test_calculate_concentration_get_molecular_properties(self):
+        """
+        Most basic functionality. 
+
+        """
+        molecules = ["no", "xyz"]
+        molecular_mass = [123, 456]
+        P = GPPM.Plume(verbose = 0, molecules = molecules, molecular_mass = molecular_mass)
+        P.calculate_concentration_get_molecular_properties()
+        self.assertTrue(numpy.allclose(P.molecular_mass, molecular_mass))
+        self.assertIsNone(P.molecular_properties)
+        
+        
+    def test_calculate_concentration_get_molecular_properties_get(self):
+        """
+        Most basic functionality. 
+
+        """
+        molecules = ["no"]
+        molecular_mass = [30]
+        P = GPPM.Plume(verbose = 0, molecules = molecules)
+        P.calculate_concentration_get_molecular_properties()
+        self.assertTrue(numpy.allclose(P.molecular_mass, molecular_mass))        
+        self.assertIsNotNone(P.molecular_properties)
         
 if __name__ == '__main__': 
     verbosity = 1
        
+    # if 1:
+        # suite = unittest.TestLoader().loadTestsFromTestCase( Test_basic )
+        # unittest.TextTestRunner(verbosity=verbosity).run(suite)    
+
+
     if 1:
-        suite = unittest.TestLoader().loadTestsFromTestCase( Test_basic )
-        unittest.TextTestRunner(verbosity=verbosity).run(suite)        
+        suite = unittest.TestLoader().loadTestsFromTestCase( Test_calculate_concentration_prepare )
+        unittest.TextTestRunner(verbosity=verbosity).run(suite)            
