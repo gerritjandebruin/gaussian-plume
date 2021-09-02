0f10a38ba19828451d866e2998a81dcf0d48b18a
Robbert Bloem <robbert.bloem@tno.nl>
2021-09-02 07:42:21 +0200

    switched lat-lon conversion factors

--- src/GaussianPlume/GPConstants.py
+++ src/GaussianPlume/GPConstants.py
@@ -1,8 +1,8 @@
 import warnings 
 import numpy
 
-latlon2dxdy_lat_conversion_factor = 6378137
-latlon2dxdy_lon_conversion_factor = 6356752
+latlon2dxdy_lon_conversion_factor = 6378137
+latlon2dxdy_lat_conversion_factor = 6356752
 
 deg2rad = numpy.pi / 180
 rad2deg = 180 / numpy.pi
